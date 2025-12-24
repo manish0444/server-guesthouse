8ed4731004802ffab14f88551fa56bc486f20d5f
manish0444 <manishbhandari0444@gmail.com>
2025-12-24 12:59:03 +0545

    df

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:8080', 'http://localhost:8080'] // Vite default ports
+  origin: ['https://krishendra-guest-house.vercel.app', 'https://krishendra-guest-house.vercel.app'] // Vite default ports
 }));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
